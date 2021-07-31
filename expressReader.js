7c449fbb4e4aa1d21be0ff724745924a1a9e1c11
Henrik <henrik.tarnanen@gmail.com>
2021-07-31 11:36:13 +0300

    initial commit

---
+++ expressReader.js
@@ -0,0 +1,47 @@
+const express = require('express')
+const cors = require('cors')
+const app = express()
+app.use(cors())
+const fs = require('fs')
+const readline = require('readline')
+
+// creates a JSON-object from the data file
+const readFileData = async (path) => {
+  const lines = []
+  const fileStream = fs.createReadStream(path)
+  const rl = readline.createInterface({
+    input: fileStream,
+    crlfDelay: Infinity
+  })
+  for await (const line of rl) {
+    lines.push(JSON.parse(line))
+  }
+  return lines
+}
+
+app.get('/', (req, res)=> {
+  res.send('Specify resource')
+})
+
+app.get(`/:resource`, async (req, res) => {
+  const resource = req.params.resource
+  const data = await readFileData(`./resources/${resource}.source`)
+  res.send(data)
+})
+
+app.get('/:resource/:id', async (req, res) => {
+  const resource = req.params.resource
+  const id = req.params.id
+  const data = await readFileData(`./resources/${resource}.source`)
+  const singleVaccination = data.find(vaccination => vaccination.id === id)
+  res.send(singleVaccination)
+})
+
+app.use((req, res, next) => {
+  res.status(404).send({ error: 'unknown endpoint' })
+})
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}...`)
+})
